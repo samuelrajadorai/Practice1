2ec0e1231a41e3039ac29e33bb2b2ee9e338e34d
Samuel <sam_jlu@yahoo.co.in>
2018-09-22 23:57:42 +0200

    Initial commit

---
+++ src/main/java/T1.java
@@ -0,0 +1,10 @@
+    public class T1 {
+
+        public static void main(String[] args) {
+
+            String a1 = "Hello!. ";
+            String a2 = "This is Sam and This is the first change";
+            System.out.println(a1 + a2);
+
+        }
+    }
